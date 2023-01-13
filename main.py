8d9d1cc021103074d9748ea007fe764e325b30b4
Przemysław Kucharczyk <przemek.franciszek.kucharczyk@gmail.com>
2023-01-13 20:57:42 +0100

    hermetization

--- main.py
+++ main.py
@@ -1,60 +1,28 @@
-from abc import ABC, abstractmethod
-class Zwierze(ABC):
-    def __init__(self, nazwa, wiek, waga):
-        self.nazwa = nazwa
+class Zwierze():
+    def __init__(self, wiek):
         self.wiek = wiek
-        self.waga = waga
-
-    @abstractmethod #tutaj wymuszamy implementację tej metody w klsach pochodnych
-    def nazwa_gatunku(self):
-        pass
-
-    def przedstaw_sie(self):
-        print(f"Jestem {self.nazwa_gatunku()}, mam na imię {self.nazwa} mam {self.wiek} lat oraz ważę {self.waga} kg.")
-
-    def urodziny(self):
-        self.wiek += 1
-
-class Slon(Zwierze):
-    def nazwa_gatunku(self):
-        return "Słoń"
-
-class Lew(Zwierze):
-    def nazwa_gatunku(self):
-        return "Lew"
-
-class Papuga(Zwierze):
-    def nazwa_gatunku(self):
-        return  "Papuga"
-    def __init__(self,nazwa, wiek, waga, kolor):
-        super().__init__(nazwa,wiek,waga)
-        self.kolor = kolor
-    def przedstaw_sie(self):
-        super().przedstaw_sie()
-        print(f"Jako papuga mój kolor to {self.kolor}")
-def nowy_rok(zoo):
-    for zwierze in zoo:
-        zwierze.urodziny()
-
-def przedstaw_zwierzeta(zoo):
-    for zwierze in zoo:
-        zwierze.przedstaw_sie()
+    @property
+    def wiek(self):
+        return self.__wiek
+    @wiek.setter
+    def wiek(self, wiek):
+        if wiek < 0:
+            self.__wiek = 0
+        elif wiek > 200:
+            self.__wiek = 200
+        else:
+            self.__wiek = wiek
 
 def main():
-    Dumboo = Slon("Dumboo", 77, 600)
-    Simba = Lew("Simba", 24, 100)
-    Jago = Papuga("Jago", 32, 3, "czerwony")
+    jakis_zwierz = Zwierze(300)
+    print(jakis_zwierz.wiek)
+    jakis_zwierz = Zwierze(-10)
+    print(jakis_zwierz.wiek)
+    jakis_zwierz = Zwierze(23)
+    print(jakis_zwierz.wiek)
 
-    Dumboo.przedstaw_sie()
-    Simba.przedstaw_sie()
-    Jago.przedstaw_sie()
 
 
-    Dumboo.urodziny()
-    Dumboo.przedstaw_sie()
-    zoo = [Dumboo, Simba, Jago]
-    nowy_rok(zoo)
-    przedstaw_zwierzeta(zoo)
 
 if __name__ == "__main__":
     main()
