248475177c5c5d692b784ff47d18bae5797b5467
Przemysław Kucharczyk <przemek.franciszek.kucharczyk@gmail.com>
2023-01-13 20:12:18 +0100

    nested inheritance

---
+++ main.py
@@ -0,0 +1,34 @@
+class Zwierze:
+    def __init__(self, nazwa, wiek, waga):
+        self.nazwa = nazwa
+        self.wiek = wiek
+        self.waga = waga
+
+    def przedstaw_sie(self):
+        print(f"Jestem zwierzakiem {self.nazwa}, mam {self.wiek} lat oraz ważę {self.waga} kg.")
+
+    def urodziny(self):
+        self.wiek += 1
+class Slon(Zwierze):
+    pass
+
+class Lew(Zwierze):
+    pass
+
+class Papuga(Zwierze):
+    pass
+
+def main():
+    Dumboo = Slon("Dumboo", 77, 600)
+    Simba = Lew("Simba", 24, 100)
+    Jago = Papuga("Jago", 32, 3)
+    jakis_zwierz = Zwierze("Matka", 54, 50)
+    Dumboo.przedstaw_sie()
+    Simba.przedstaw_sie()
+    Jago.przedstaw_sie()
+    jakis_zwierz.przedstaw_sie()
+
+    Dumboo.urodziny()
+    Dumboo.przedstaw_sie()
+if __name__ == "__main__":
+    main()
